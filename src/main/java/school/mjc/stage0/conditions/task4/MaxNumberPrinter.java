1d35d91f3ad35a4bcd8f31224abf0e8ce410be9e
Sergey Khaykov <ssserg3000@gmail.com>
2022-11-25 20:02:05 +0200

    initial commit

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        if (first>second){
+            if (first>third){
+                System.out.println("first");
+            }
+            else if (third>first){
+                System.out.println("third");
+            }
+        }
+        else {
+            System.out.println("second");
+        }
+    }
+
+    public static void main(String[] args) {
+        MaxNumberPrinter mnp = new MaxNumberPrinter();
+        mnp.printGreatest(13,1,19);
     }
 }
